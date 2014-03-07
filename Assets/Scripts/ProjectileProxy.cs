470a332e6b22c7ec8cb35a3be29e9d35bfc7a2a7
Nicholas Hughes <hughes.nick@gmail.com>
2014-03-06 18:42:47 -0800

    Add slingshot and projectile proxy scripts

---
+++ Assets/Scripts/ProjectileProxy.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ProjectileProxy : MonoBehaviour {
+
+  private SlingshotProxy slingshot;
+  private uLinkSmoothRigidbodyImproved smoothRigidbody;
+
+  public void Loosen(){
+    Release();
+    enableCollider();
+    rigidbody.mass = 20;
+  }
+
+  public void Release(){
+    transform.parent = null;
+    smoothRigidbody.enabled = true;
+    rigidbody.interpolation = RigidbodyInterpolation.Interpolate;
+    rigidbody.constraints = RigidbodyConstraints.None;
+    rigidbody.isKinematic = false;
+    Invoke("enableCollider", .25f);
+  }
+
+  private void enableCollider(){
+    collider.enabled = true;
+  }
+
+  void uLink_OnNetworkInstantiate(uLink.NetworkMessageInfo info){
+    int ownerViewId = info.networkView.initialData.Read<int>();
+    uLink.NetworkView slingshotNetworkView = uLink.NetworkView.Find(new uLink.NetworkViewID(ownerViewId));
+    slingshot = slingshotNetworkView.GetComponentInChildren<SlingshotProxy>();
+    slingshot.SetProjectile(this);
+    smoothRigidbody = GetComponent<uLinkSmoothRigidbodyImproved>();
+    disablePhysics();
+    transform.parent = slingshot.transform;
+    transform.position = slingshot.transform.position;
+  }
+
+  private void disablePhysics(){
+    rigidbody.interpolation = RigidbodyInterpolation.None;
+    rigidbody.isKinematic = true;
+    collider.enabled = false;
+    smoothRigidbody.enabled = false;
+  }
+}
---
+++ Assets/Scripts/SlingshotProxy.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SlingshotProxy : MonoBehaviour {
+
+  private bool deactivated = false;
+  private ProjectileProxy projectile;
+
+  void Start () {
+  
+  }
+  
+  void Update () {
+  
+  }
+
+  public void Deactivate(){
+    deactivated = true;
+    if (projectile) projectile.Loosen();
+  }
+
+  public void Aim(Quaternion rotation){
+    transform.rotation = Quaternion.Slerp(transform.rotation, rotation, 25f * Time.deltaTime);
+  }
+
+  public void SetProjectile(ProjectileProxy projectile){
+    this.projectile = projectile;
+  }
+}
